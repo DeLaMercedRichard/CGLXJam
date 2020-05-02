8e990150cb56894bc75c4f9c915225c8384c6929
DeLaMercedRichard <delamercedrichard@gmail.com>
2020-05-01 22:47:19 -0400

    Merged Completed

--- CGXJam/Intermediate/Build/Win64/UE4Editor/Inc/CGXJam/CGXJam.init.gen.cpp
+++ CGXJam/Intermediate/Build/Win64/UE4Editor/Inc/CGXJam/CGXJam.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeCGXJam_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x129E2034,
+				0xF0804F51,
 				0x7E7C0ACF,
 				METADATA_PARAMS(nullptr, 0)
 			};
