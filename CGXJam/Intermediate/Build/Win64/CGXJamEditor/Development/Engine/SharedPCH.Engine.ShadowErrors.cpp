e1ccd5e283f787cade28fcdff386e609333e7b25
DeLaMercedRichard <delamercedrichard@gmail.com>
2020-05-02 21:46:23 -0400

    Assets Updated Player Spawn and All other Asset Organization Final C++ Compilation No Errors

--- CGXJam/Intermediate/Build/Win64/CGXJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ CGXJam/Intermediate/Build/Win64/CGXJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Brand/Desktop/CGLXJam/CGXJam/Intermediate/Build/Win64/CGXJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/Delam/OneDrive/Documents/GitHub/CGLXJam/CGXJam/Intermediate/Build/Win64/CGXJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
